9e89a6252a43789adcf53fa4707977507f4ccc23
Satya Prakash <75464746+Satya03012002@users.noreply.github.com>
2023-03-02 15:33:44 +0530

    Update conn.js

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+//
 const CONNECTION_URL =  "mongodb+srv://traveldairies:123123123@cluster0.7qel1.mongodb.net/mernyoutube?retryWrites=true&w=majority";
 
 mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
