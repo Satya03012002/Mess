f71209a6b008ace2870c61936689ace3c3b8dc2c
Satya Prakash <75464746+Satya03012002@users.noreply.github.com>
2023-05-20 00:37:54 +0530

    Update conn.js

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 //
-const CONNECTION_URL =  "mongodb+srv://traveldairies:123123123@cluster0.7qel1.mongodb.net/mernyoutube?retryWrites=true&w=majority";
+const CONNECTION_URL =  "mongodb+srv://${process.env.DBNAME}:${process.env.PASSWORD}@cluster0.7qel1.mongodb.net/mernyoutube?retryWrites=true&w=majority";
 
 mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
 .then(()=>console.log("mongodb connection successful"))
